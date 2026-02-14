57d39d69ecf8e5819e77ef77bf4a4eeff674b04d
Rob Muhlestein <rob@rwx.gg>
2026-02-14 18:52:16 -0500

    working initial python scaffoldign

--- pylib/pylib.py
+++ pylib/pylib.py
@@ -9,7 +9,6 @@ exe = pathlib.Path(sys.argv[0]).resolve()
 base = exe.parent
 
 def applies_to_host(item):
-    print("made")
     for host in item["hosts"]:
         if host == "localhost" or host == hostname:
             return True
